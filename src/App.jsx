1a1aa8b11ed1d1a8bdda32c9ed59fa726fd09119
Rishav <rishavkumarsharma03@gmail.com>
2024-09-13 16:30:16 +0530

    rfc

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useForm } from 'react-hook-form'
 
 const App = () => {
 const {register,handleSubmit,reset} = useForm();
+const [formData, setFormData] = useState(null)
 //console.log(register())
 const onSub = (data) => {
+
   console.log(data); // Handle form data as needed
   reset(); // Reset form fields to their default values
+  setFormData(data)
 };
 
   return (
@@ -15,7 +18,18 @@ const onSub = (data) => {
       <input {...register('name')} type='text' placeholder='Name' />
       <input {...register('email')} type='email' placeholder='Email' />
       <input type='submit' />
+     
       </form>
+      <div>
+      {formData && (
+        <div>
+          <h2>Submitted Data:</h2>
+          <p><strong>Name:</strong> {formData.name}</p>
+          <p><strong>Email:</strong> {formData.email}</p>
+        </div>
+      )}
+      </div>
+      
     </div>
   )
 }
---
+++ src/components/Dsa.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function dsa() {
+  return (
+    <div>
+      <h1>hi</h1>
+    </div>
+  )
+}
+
+export default dsa
